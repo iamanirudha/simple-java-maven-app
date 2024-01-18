eb555f9cf694225cd136181c811d0bf2721877e5
iamanirudha <ani3891@gmail.com>
2024-01-18 21:13:32 +0530

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -20,5 +20,5 @@ echo "Branch Name without prefix: $branchName"
 echo "Deploy To: $DEPLOY_TO"
 
 # Export DEPLOY_TO variable for Jenkins to use
-echo "export DEPLOY_TO=$DEPLOYMENT_ENV" > env.properties
+echo "export DEPLOYMENT_ENV=$DEPLOY_TO" > env.properties
 echo "export BRANCH_NAME=$branchName" >> env.properties
